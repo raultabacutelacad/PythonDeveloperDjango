771014cdf7cb3511909c972277ce4e33fde3ee45
student <student@telacad.ro>
2024-01-15 19:19:22 +0200

    database added

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoSite2.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoSite.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
